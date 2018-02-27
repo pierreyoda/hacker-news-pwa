759337c0a5f7a61d2f86f119065a1f00db801886
pierreyoda <pierreyoda@users.noreply.github.com>
2018-02-27 18:32:55 +0100

    Quasar v0.15 : initial project setup
    For v0.15, the Quasar CLI and starter kit was completely rewritten.

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,14 @@
 <template>
-    <q-layout ref="layout">
-        <q-toolbar slot="header">
-            <q-btn flat @click="$refs.layout.toggleLeft()">
-                <q-icon name="menu" />
-            </q-btn>
-            <q-toolbar-title>
-                Hacker News
-                <span slot="subtitle">Simple PWA Reader</span>
-            </q-toolbar-title>
-        </q-toolbar>
-
-        <q-tabs slot="navigation" v-if="layoutTabsVisible">
-            <q-route-tab slot="title" to="/top" replace label="Top stories" />
-            <q-route-tab slot="title" to="/new" replace label="Newest stories" />
-            <q-route-tab slot="title" to="/best" replace label="Best stories" />
-        </q-tabs>
-
-        <q-scroll-area slot="left" class="panel">
-            <q-list-header>Left Panel</q-list-header>
-            <q-side-link item to="/">
-                <q-item-side icon="home" />
-                <q-item-main label="Home" sublabel="Hacker News Stories" />
-            </q-side-link>
-            <q-side-link item to="/settings">
-                <q-item-side icon="settings" />
-                <q-item-main label="Settings" />
-            </q-side-link>
-        </q-scroll-area>
-
-        <main>
-            <router-view></router-view>
-        </main>
-    </q-layout>
+  <div id="q-app">
+    <router-view />
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import {
-    QLayout,
-    QToolbar,
-    QToolbarTitle,
-    QTabs,
-    QRouteTab,
-    QScrollArea,
-    QListHeader,
-    QSideLink,
-    QItemMain,
-    QItemSide,
-    QBtn,
-    QIcon,
-} from "quasar";
-import { Getter } from "vuex-class";
-import Component from "vue-class-component";
-
-/*
- * Root component
- */
-@Component({
-    name: "app",
-    components: {
-        QLayout,
-        QToolbar,
-        QToolbarTitle,
-        QTabs,
-        QRouteTab,
-        QScrollArea,
-        QListHeader,
-        QSideLink,
-        QItemMain,
-        QItemSide,
-        QBtn,
-        QIcon,
-    }
-})
-export default class App extends Vue {
-    @Getter("app/tabsVisible") layoutTabsVisible: any;
+<script>
+export default {
+  name: 'App'
 }
 </script>
 
-<style scoped lang="stylus">
-@import "~variables"
-
-.panel
-    width 100%
-    height 100%
-
-main
-    text-align center
-    max-width $mainMaxWidth
-    margin 20px auto 0
-    background-color #eeeee3
+<style>
 </style>
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { registerFilters } from "./filters";
-
-export const pluralize = (list: any[], singular: string, plural: string): string =>
-    list.length > 1 ? plural : singular;
-;
